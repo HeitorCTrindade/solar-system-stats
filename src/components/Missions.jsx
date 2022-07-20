471937d2b638085c64b3e1a7092b6ac3fbb329d8
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-07-20 16:33:07 -0300

    Requisito 10 - finalziado | SolarSystem.jsx refatorado para melhor leitura.

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
+    const missionsCardsComponents = missions.map((mission, key) => (
+      <MissionCard
+        key={ mission.name }
+        name={ mission.name }
+        year={ mission.year }
+        country={ mission.country }
+        destination={ mission.destination }
+      />
+    ));
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { missionsCardsComponents }
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -5,13 +5,17 @@ import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
-    const planetsComponent = planets.map((planet, key) => (
-      <PlanetCard key={ key } planetName={ planet.name } planetImage={ planet.image } />
+    const planetsComponents = planets.map((planet, key) => (
+      <PlanetCard
+        key={ planet.name }
+        planetName={ planet.name }
+        planetImage={ planet.image }
+      />
     ));
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        { planetsComponent }
+        { planetsComponents }
       </div>
     );
   }
