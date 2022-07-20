75da4bc2923413b936a14a0b9c07a7f8bdba2553
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-07-20 16:09:46 -0300

    Requisito 07 - complementado e finalizado | Requisito 08 - finalizado.

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Title from './Title';
 
 class Missions extends React.Component {
   render() {
-    const { planetName, planetImage } = this.props;
     return (
       <div data-testid="missions">
-        <Title />
+        <Title headline="Missões" />
       </div>
     );
   }
 }
-
-Missions.propTypes = {
-  planetName: PropTypes.string.isRequired,
-  planetImage: PropTypes.string.isRequired,
-};
 
 export default Missions;
